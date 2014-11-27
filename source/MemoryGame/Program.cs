72217e1799e19008af7091d7807442d3a8e00a6c
d95ebj <ebj@ljusberg.se>
2014-11-27 13:05:13 +0100

    Revert "StartForm Design"
    This reverts commit 1e78dcb6998d054a45fb6bc2632fb568458afc5a.

--- source/MemoryGame/Program.cs
+++ source/MemoryGame/Program.cs
@@ -16,7 +16,7 @@ namespace MemoryGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StartForm());
+            Application.Run(new Form1());
         }
     }
 }
