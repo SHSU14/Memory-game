0398e18a3fd52bd50d41cb3d4e61d56b8908b059
skwebies <skwebbies@Thineskumars-MacBook-Pro.local>
2014-11-27 14:55:21 +0100

    Startform

--- source/MemoryGame/StartForm.cs
+++ source/MemoryGame/StartForm.cs
@@ -16,25 +16,5 @@ namespace MemoryGame
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
