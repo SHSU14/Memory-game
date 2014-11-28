aacaa79a8e03fd0ae61dd217a70971fb2703f3da
marjan21 <marcus_jansson91@hotmail.com>
2014-11-28 12:18:49 +0100

    Städning

--- source/MemoryGame/Program.cs
+++ source/MemoryGame/Program.cs
@@ -16,8 +16,8 @@ namespace MemoryGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            Application.Run(new GameBoardForm());
+            Application.Run(new Form1());
+          
         }
     }
 }
