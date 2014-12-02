8db31a62ae03597ac6e9bdb43b8f82d87580fe83
marjan21 <marcus_jansson91@hotmail.com>
2014-12-02 11:32:08 +0100

    Funktioner för Settings

--- source/MemoryGame/StartForm.cs
+++ source/MemoryGame/StartForm.cs
@@ -12,7 +12,9 @@ namespace MemoryGame
 {
     public partial class StartForm : Form
     {
-      
+        public Settings settings = new Settings();
+        
+
         public StartForm()
         {
             InitializeComponent();
@@ -21,6 +23,8 @@ namespace MemoryGame
         // Öppna spelplanen(GameBoardForm)
         private void btn_starta_Click(object sender, EventArgs e)
         {
+            getSettings();    
+         
             GameBoardForm frm = new GameBoardForm(this);
             frm.Show();
             this.Hide();
@@ -30,5 +34,37 @@ namespace MemoryGame
         {
            return MessageBox.Show("Vill du verkligen avsluta?", string.Empty, MessageBoxButtons.YesNo, MessageBoxIcon.Question);   
         }
+
+        public void getSettings()
+        {
+            var value = this.cmb_spelare.SelectedItem.ToString();
+            settings.Players = int.Parse(value);
+           
+
+            var value1 = this.cmb_AI.SelectedItem.ToString();
+            settings.AIPlayers = int.Parse(value1);
+
+
+            var value2 = this.cmb_Level.Text;
+            settings.AILevels = value2;
+   
+
+            var value3 = this.num_Cardnumber.Value.ToString();
+            settings.CardNumber = int.Parse(value3);
+        
+
+            var value4 = this.cmb_Themes.Text;
+            settings.Themes = value4;
+           
+
+            var value5 = this.nud_PlayerTurnTimer.Value.ToString();
+            settings.PlayersTurnTimer = int.Parse(value5);
+           
+
+            var value6 = this.nud_ShowCardTimer.Value.ToString();
+            settings.CardNumber = int.Parse(value6);
+          
+        }
+
     }
 }
