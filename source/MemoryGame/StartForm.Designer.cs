d89ae08bc125bf0a504dced6349abd3e66d4a181
d95ebj <ebj@ljusberg.se>
2014-12-03 21:44:43 +0100

    UI-fixar

--- source/MemoryGame/StartForm.Designer.cs
+++ source/MemoryGame/StartForm.Designer.cs
@@ -50,7 +50,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(156, 254);
+            this.label5.Location = new System.Drawing.Point(79, 238);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(86, 13);
@@ -60,7 +60,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(156, 229);
+            this.label4.Location = new System.Drawing.Point(79, 213);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 13);
@@ -69,7 +69,7 @@
             // 
             // nud_ShowCardTimer
             // 
-            this.nud_ShowCardTimer.Location = new System.Drawing.Point(265, 250);
+            this.nud_ShowCardTimer.Location = new System.Drawing.Point(188, 234);
             this.nud_ShowCardTimer.Margin = new System.Windows.Forms.Padding(2);
             this.nud_ShowCardTimer.Name = "nud_ShowCardTimer";
             this.nud_ShowCardTimer.Size = new System.Drawing.Size(71, 20);
@@ -82,7 +82,7 @@
             // 
             // nud_PlayerTurnTimer
             // 
-            this.nud_PlayerTurnTimer.Location = new System.Drawing.Point(265, 225);
+            this.nud_PlayerTurnTimer.Location = new System.Drawing.Point(188, 209);
             this.nud_PlayerTurnTimer.Margin = new System.Windows.Forms.Padding(2);
             this.nud_PlayerTurnTimer.Name = "nud_PlayerTurnTimer";
             this.nud_PlayerTurnTimer.Size = new System.Drawing.Size(71, 20);
@@ -100,7 +100,7 @@
             0,
             0,
             0});
-            this.num_Cardnumber.Location = new System.Drawing.Point(236, 158);
+            this.num_Cardnumber.Location = new System.Drawing.Point(159, 142);
             this.num_Cardnumber.Margin = new System.Windows.Forms.Padding(2);
             this.num_Cardnumber.Maximum = new decimal(new int[] {
             120,
@@ -113,7 +113,7 @@
             0,
             0});
             this.num_Cardnumber.Name = "num_Cardnumber";
-            this.num_Cardnumber.Size = new System.Drawing.Size(100, 20);
+            this.num_Cardnumber.Size = new System.Drawing.Size(49, 20);
             this.num_Cardnumber.TabIndex = 18;
             this.num_Cardnumber.Value = new decimal(new int[] {
             12,
@@ -138,10 +138,10 @@
             "8",
             "9",
             "10"});
-            this.cmb_spelare.Location = new System.Drawing.Point(236, 50);
+            this.cmb_spelare.Location = new System.Drawing.Point(159, 34);
             this.cmb_spelare.Margin = new System.Windows.Forms.Padding(2);
             this.cmb_spelare.Name = "cmb_spelare";
-            this.cmb_spelare.Size = new System.Drawing.Size(102, 21);
+            this.cmb_spelare.Size = new System.Drawing.Size(49, 21);
             this.cmb_spelare.TabIndex = 17;
             this.cmb_spelare.Text = "2";
             this.cmb_spelare.SelectedIndexChanged += new System.EventHandler(this.cmb_spelare_SelectedIndexChanged);
@@ -163,10 +163,10 @@
             "8",
             "9",
             "10"});
-            this.cmb_AI.Location = new System.Drawing.Point(236, 92);
+            this.cmb_AI.Location = new System.Drawing.Point(159, 76);
             this.cmb_AI.Margin = new System.Windows.Forms.Padding(2);
             this.cmb_AI.Name = "cmb_AI";
-            this.cmb_AI.Size = new System.Drawing.Size(102, 21);
+            this.cmb_AI.Size = new System.Drawing.Size(49, 21);
             this.cmb_AI.TabIndex = 16;
             this.cmb_AI.Text = "0";
             this.cmb_AI.SelectedIndexChanged += new System.EventHandler(this.cmb_AI_SelectedIndexChanged);
@@ -175,7 +175,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(156, 160);
+            this.label3.Location = new System.Drawing.Point(79, 144);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 13);
@@ -185,7 +185,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(156, 92);
+            this.label2.Location = new System.Drawing.Point(79, 76);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
@@ -195,7 +195,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(156, 54);
+            this.label1.Location = new System.Drawing.Point(79, 38);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
@@ -204,7 +204,7 @@
             // 
             // btn_starta
             // 
-            this.btn_starta.Location = new System.Drawing.Point(236, 291);
+            this.btn_starta.Location = new System.Drawing.Point(159, 275);
             this.btn_starta.Margin = new System.Windows.Forms.Padding(2);
             this.btn_starta.Name = "btn_starta";
             this.btn_starta.Size = new System.Drawing.Size(100, 33);
@@ -216,7 +216,7 @@
             // Tema
             // 
             this.Tema.AutoSize = true;
-            this.Tema.Location = new System.Drawing.Point(156, 196);
+            this.Tema.Location = new System.Drawing.Point(79, 180);
             this.Tema.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Tema.Name = "Tema";
             this.Tema.Size = new System.Drawing.Size(37, 13);
@@ -225,35 +225,39 @@
             // 
             // cmb_Level
             // 
+            this.cmb_Level.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmb_Level.FormattingEnabled = true;
             this.cmb_Level.Items.AddRange(new object[] {
             "Lätt",
             "Medel ",
             "Svår"});
-            this.cmb_Level.Location = new System.Drawing.Point(236, 112);
+            this.cmb_Level.SelectedIndex = 0;
+            this.cmb_Level.Location = new System.Drawing.Point(159, 96);
+            this.cmb_Level.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.cmb_Level.Name = "cmb_Level";
             this.cmb_Level.Size = new System.Drawing.Size(104, 21);
             this.cmb_Level.TabIndex = 31;
-            this.cmb_Level.Text = "Nivå";
             // 
             // cmb_Themes
             // 
+            this.cmb_Themes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmb_Themes.FormattingEnabled = true;
             this.cmb_Themes.Items.AddRange(new object[] {
             "Frukt",
             "Vägmärken",
             "Djur"});
-            this.cmb_Themes.Location = new System.Drawing.Point(236, 192);
+            this.cmb_Themes.SelectedIndex = 0;
+            this.cmb_Themes.Location = new System.Drawing.Point(159, 176);
+            this.cmb_Themes.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.cmb_Themes.Name = "cmb_Themes";
-            this.cmb_Themes.Size = new System.Drawing.Size(121, 21);
+            this.cmb_Themes.Size = new System.Drawing.Size(104, 21);
             this.cmb_Themes.TabIndex = 32;
-            this.cmb_Themes.Text = "Frukt";
             // 
             // StartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 373);
+            this.ClientSize = new System.Drawing.Size(370, 361);
             this.Controls.Add(this.cmb_Themes);
             this.Controls.Add(this.cmb_Level);
             this.Controls.Add(this.Tema);
@@ -268,8 +272,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "StartForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Memory ";
             ((System.ComponentModel.ISupportInitialize)(this.nud_ShowCardTimer)).EndInit();
